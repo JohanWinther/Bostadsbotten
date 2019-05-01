a660029a5a615767e242cdd6dc53156d10b41381
JohanWinther <johan.winther@outlook.com>
2019-05-01 15:03:45 +0200

    Remove images upon update

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const config = require('./config.json')
-const sites = require('./credentials.json')
 const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs');
+
+const config = require('./config.json');
+const sites = require('./credentials.json');
+
+const directory = path.resolve(__dirname, './');
+const imagesDirectory = path.join(directory, 'images');
+
+fs.readdir(imagesDirectory, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+        fs.unlink(path.join(imagesDirectory, file), err => {
+            if (err) throw err;
+        });
+    }
+});
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     for (site in sites) {
+        console.log(`Opening ${config[site].name}`);
         await page.goto(config[site].url);
         for (step of config[site].steps) {
             if (step.action === 'type') {
@@ -18,7 +34,7 @@ const puppeteer = require('puppeteer');
             }
             await page.waitFor(1);
         }
-        await page.screenshot({path: path.join(path.resolve(__dirname, './'), `images/${config[site].name}.png`)});
+        await page.screenshot({path: path.join(directory, `images/${config[site].name}.png`)});
     }
     await browser.close();
 })();
